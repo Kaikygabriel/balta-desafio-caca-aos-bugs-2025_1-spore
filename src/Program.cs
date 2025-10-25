9a8dad926ed3e21441e29fe13cb6ad0a665f51bc
Kaiky gabriel alves <kaikygabrielalves707@gmail.com>
2025-10-25 10:52:14 -0300

    Desafio finalizado

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,10 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(x =>
+    x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
